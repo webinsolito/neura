358dc0a9d858ea21db4608940855df774cc75b9e
webinsolito <jpagenzia@gmail.com>
2026-09-21 18:02:52 +0200

    c9.53: add poisoned embedding fallback tests

--- capo_local/overlay_c9_53/local_embedding_test.go
+++ capo_local/overlay_c9_53/local_embedding_test.go
@@ -3,18 +3,45 @@ package main
 import (
 	"context"
 	"errors"
+	"math"
 	"testing"
 	"time"
 )
 
-type fakeEmbeddingProvider struct { v []float32; err error }
-func (f fakeEmbeddingProvider) Embed(context.Context,string)([]float32,error){ return f.v,f.err }
-
-func TestBuildMemoryQueryEmbeddingHealthy(t *testing.T){
-	q:=BuildMemoryQuery(context.Background(),fakeEmbeddingProvider{v:[]float32{1,2}},"x",nil,time.Time{},3)
-	if len(q.Embedding)!=2 { t.Fatalf("embedding not attached") }
+type fakeEmbeddingProvider struct {
+	v []float32
+	err error
 }
-func TestBuildMemoryQueryEmbeddingFailureFallsBack(t *testing.T){
-	q:=BuildMemoryQuery(context.Background(),fakeEmbeddingProvider{err:errors.New("offline")},"x",nil,time.Time{},3)
-	if len(q.Embedding)!=0 { t.Fatalf("expected lexical fallback") }
+
+func (f fakeEmbeddingProvider) Embed(context.Context, string) ([]float32, error) {
+	return f.v, f.err
+}
+
+func TestBuildMemoryQueryEmbeddingHealthy(t *testing.T) {
+	q := BuildMemoryQuery(context.Background(), fakeEmbeddingProvider{v: []float32{1, 2}}, "x", nil, time.Time{}, 3)
+	if len(q.Embedding) != 2 {
+		t.Fatalf("embedding not attached")
+	}
+}
+
+func TestBuildMemoryQueryEmbeddingFailureFallsBack(t *testing.T) {
+	q := BuildMemoryQuery(context.Background(), fakeEmbeddingProvider{err: errors.New("offline")}, "x", nil, time.Time{}, 3)
+	if len(q.Embedding) != 0 {
+		t.Fatalf("expected lexical fallback")
+	}
+}
+
+func TestBuildMemoryQueryRejectsNonFiniteProviderEmbedding(t *testing.T) {
+	q := BuildMemoryQuery(context.Background(), fakeEmbeddingProvider{v: []float32{1, float32(math.NaN())}}, "x", nil, time.Time{}, 3)
+	if len(q.Embedding) != 0 {
+		t.Fatalf("poisoned embedding attached: %#v", q.Embedding)
+	}
+}
+
+func TestBuildMemoryQueryRejectsOversizedProviderEmbedding(t *testing.T) {
+	huge := make([]float32, maxLocalEmbeddingDimensions+1)
+	q := BuildMemoryQuery(context.Background(), fakeEmbeddingProvider{v: huge}, "x", nil, time.Time{}, 3)
+	if len(q.Embedding) != 0 {
+		t.Fatalf("oversized embedding attached: %d", len(q.Embedding))
+	}
 }
