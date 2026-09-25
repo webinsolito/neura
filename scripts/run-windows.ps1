123d61529d2edf458eac9aaba70b82c36f0cb7b0
webinsolito <jpagenzia@gmail.com>
2026-09-25 17:11:40 +0200

    feat: route local Windows run through packaged launcher

--- scripts/run-windows.ps1
+++ scripts/run-windows.ps1
@@ -1,15 +1,20 @@
 $ErrorActionPreference = 'Stop'
 
 $RepoRoot = Split-Path -Parent $PSScriptRoot
-$Exe = Join-Path $RepoRoot 'dist\neura.exe'
+$PackageDir = Join-Path $RepoRoot 'dist\NEURA-Windows'
+$Exe = Join-Path $PackageDir 'neura.exe'
+$Launcher = Join-Path $PackageDir 'Start-NEURA.ps1'
 
-if (-not (Test-Path $Exe)) {
+if (-not (Test-Path -LiteralPath $Exe -PathType Leaf) -or -not (Test-Path -LiteralPath $Launcher -PathType Leaf)) {
     & (Join-Path $PSScriptRoot 'build-windows.ps1')
 }
 
-if (-not (Test-Path $Exe)) {
+if (-not (Test-Path -LiteralPath $Exe -PathType Leaf)) {
     throw 'NEURA executable was not produced.'
 }
+if (-not (Test-Path -LiteralPath $Launcher -PathType Leaf)) {
+    throw 'NEURA launcher was not produced.'
+}
 
-& $Exe @args
+& $Launcher @args
 exit $LASTEXITCODE
